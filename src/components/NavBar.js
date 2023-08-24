a926cb9cc3b33dc493b2a02cc8549e0e83ae67ca
zewdu erkyhun <zewdu444@gmail.com>
2023-08-24 21:31:10 +0300

    Add navbar to register and detail page

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,57 @@
-import React from 'react'
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import HomeIcon from '@mui/icons-material/Home';
+import { Link, useLocation } from 'react-router-dom'
 
 function NavBar() {
+  const location = useLocation();
   return (
-    <div>NavBar</div>
+    <Box sx={{ flexGrow: 1 }}>
+    <AppBar position="static">
+      <Toolbar>
+      {location.pathname === '/register' ?
+      <IconButton
+      size="large"
+      edge="start"
+      color="inherit"
+      aria-label="menu"
+      sx={{ mr: 2 }} >
+      <HomeIcon />
+    </IconButton> :
+    <IconButton
+    component={Link}
+    to="/register"
+    size="large"
+    edge="start"
+    color="inherit"
+    aria-label="menu"
+    sx={{ mr: 2 }} >
+    <ArrowBackIcon />
+  </IconButton> }
+        <Typography variant="h6" component="div" sx={{
+          flexGrow: 1,
+          textAlign: 'center',
+        }}>
+          {location.pathname === '/register' ? 'Register' : 'Student Details'}
+        </Typography>
+        <IconButton
+        size="large"
+        edge="start"
+        color="inherit"
+        aria-label="menu"
+        sx={{ ml: 2 }}
+      >
+        <MenuIcon />
+      </IconButton>
+      </Toolbar>
+    </AppBar>
+  </Box>
   )
 }
 
--- src/components/Splash.js
+++ src/components/Splash.js
@@ -1,8 +1,17 @@
+import { Button, Stack, Typography } from '@mui/material'
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 function Splash() {
   return (
-    <div>splash</div>
+    <Stack sx={{
+      display: 'flex',
+      flexDirection: 'column',
+       alignItems: 'center',
+        paddingTop: '15rem',
+    }}>
+      <Typography variant="h1" sx={{ fontSize: '2rem', textAlign: 'center'}}>Welcome to Student Enrollment</Typography>
+      <Button variant="contained" component={Link} to="/register" sx={{ margin: '0 auto', marginTop: '2rem', width: '10rem', height: '3rem', fontSize: '1.2rem' }}>Register</Button>
+    </Stack>
   )
 }
 
